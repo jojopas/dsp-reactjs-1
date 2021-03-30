ec98e617c743e699edd87a622f17f7bb122b2456
Your Name <you@example.com>
2021-03-30 14:05:46 +0530

    minor fix

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ import Page from "./Page";
 import PDP from "./PDP";
 import Promotions from "./Promotions";
 import Rails from "./Rails";
-import Recommendation from "./Recommendation";
+import Recommendation from "./Recommendation.js";
 import Search from "./Search";
 
 export { EPG, Genres, Page, PDP, Promotions, Rails, Recommendation,  Search };
