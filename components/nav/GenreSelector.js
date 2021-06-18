6d843b40f1c36bcd1e42e56a465b8740833b1245
Piyush Mishra <piyush.mishra@rsystems.com>
2021-06-18 18:45:29 +0530

    Style Change

--- components/nav/GenreSelector.js
+++ components/nav/GenreSelector.js
@@ -1,49 +1,59 @@
-import React from 'react';
-import {useRouter} from 'next/router';
+import React from "react";
+import { useRouter } from "next/router";
 
-import './GenreSelector.less';
-import InlineSVG from '../InlineSVG';
-import { titleCase } from '../../helpers/utils/strings';
+import "./GenreSelector.less";
+import InlineSVG from "../InlineSVG";
+import { titleCase } from "../../helpers/utils/strings";
 
-export default function GenreSelector({className, style, links, type, changeListener, hoveredListener}) {
-
+export default function GenreSelector({
+    className,
+    style,
+    links,
+    type,
+    changeListener,
+    hoveredListener,
+}) {
+    if (!links) {
+        return <></>;
+    }
     const router = useRouter();
     const [currentGenre, setCurrentGenre] = React.useState(null);
     const [isHovered, setIsHovered] = React.useState(false);
 
-    let currName = (type === 'live') ? 'All Channels' : 'All Genres';
-    if(type !== 'live' && router.asPath !== `/${type}`) {
-        const currLink = links.find(el => el.as === router.asPath);
-        currName = `${currLink.inner} ${titleCase(type)}`;
+    let currName = type === "live" ? "All Channels" : "All Genres";
+    if (type !== "live" && router.asPath !== `/${type}`) {
+        const currLink = links.find((el) => el.as === router.asPath);
+        currName = `${currLink?.inner} ${titleCase(type)}`;
     }
 
     const [currentName, setCurrentName] = React.useState(currName);
 
     React.useEffect(() => {
-        if(type === 'live') {
-
+        if (type === "live") {
         } else {
             setCurrentGenre(router.asPath);
 
             if (router.asPath !== `/${type}`) {
-                const currLink = links.find(el => el.as === router.asPath);
+                const currLink = links.find((el) => el.as === router.asPath);
                 setCurrentName(`${currLink.inner} ${titleCase(type)}`);
             }
         }
     }, [currentGenre, router.asPath]);
 
     React.useEffect(() => {
-        if(hoveredListener) {
+        if (hoveredListener) {
             hoveredListener(isHovered);
         }
     }, [isHovered]);
 
     const handleChange = (e) => {
         e.preventDefault();
-        if(type === 'live') {
+        if (type === "live") {
             setCurrentName(e.target.options[e.target.selectedIndex].text);
             setCurrentGenre(e.target.value);
-            changeListener(e.target.value === 'All Genres' ? '' : e.target.value);
+            changeListener(
+                e.target.value === "All Genres" ? "" : e.target.value
+            );
             setIsHovered(false);
         } else {
             let url = `/${type}`;
@@ -52,27 +62,47 @@ export default function GenreSelector({className, style, links, type, changeList
             }
             router.push(url, e.target.value);
         }
-    }
+    };
 
     return (
-        <span className={className ? `genreSelector ${className}` : 'genreSelector'} style={style}>
+        <span
+            className={
+                className ? `genreSelector ${className}` : "genreSelector"
+            }
+            style={style}
+        >
             <select
                 onChange={handleChange}
-                value={type === 'live' ? currentGenre || '' : currentGenre || `/${type}`}
+                value={
+                    type === "live"
+                        ? currentGenre || ""
+                        : currentGenre || `/${type}`
+                }
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
             >
-                <option key='all-genres' value={type === 'live' ? '' : `/${type}`}>
-                    {type === 'live' ? 'All Channels' : 'All Genres'}
+                <option
+                    key="all-genres"
+                    value={type === "live" ? "" : `/${type}`}
+                >
+                    {type === "live" ? "All Channels" : "All Genres"}
                 </option>
                 {links.map((link) => (
-                    <option key={link.id} value={link.as}>{link.inner}</option>
+                    <option key={link.id} value={link.as}>
+                        {link.inner}
+                    </option>
                 ))}
             </select>
-            <span className={`genreSelector-selected${isHovered ? ' genreSelector-hover' : ''}`}>
+            <span
+                className={`genreSelector-selected${
+                    isHovered ? " genreSelector-hover" : ""
+                }`}
+            >
                 {currentName}
-                <span className="genreSelector-arrow"><InlineSVG type="arrow"/></span>
+                <span className="genreSelector-arrow">
+                    <InlineSVG type="arrow" />
+                </span>
             </span>
         </span>
-    )
+    );
 }
---
+++ pages/on-demand/index.js
@@ -0,0 +1,178 @@
+import React from "react";
+import { useObserver } from "mobx-react-lite";
+
+import getSession from "../../helpers/session/get-session";
+import pageError from "../../helpers/page/error";
+import { getLayout } from "../../components/Layout";
+import { StoreContext } from "../../store";
+import { pageBuilder } from "../../helpers/page/builder";
+import Error404 from "../../components/404";
+import CardList from "../../components/cardList/CardList";
+import Carousel from "../../components/carousel/Carousel";
+import Button from "../../components/button/Button";
+import { constants } from "../../config";
+import InlineSVG from "../../components/InlineSVG";
+import GenreSelector from "../../components/nav/GenreSelector";
+import { slugify } from "../../helpers/utils/strings";
+
+import "./index.less";
+
+export default function Home({
+    session,
+    config,
+    page,
+    error,
+    pageType,
+    seoObj,
+}) {
+    const [width, setWidth] = React.useState();
+    const [clickedCardTitle, setClickedCardTitle] = React.useState();
+    const genreNav = page.genres?.map((genre) => ({
+            id: genre,
+            inner: genre,
+            url: `/movies/genre/[...slug]`,
+            as: `/movies/genre/${slugify(genre)}`,        
+    }));
+
+    React.useEffect(() => {
+        if (!width && typeof window !== "undefined") {
+            setWidth(window.innerWidth);
+        }
+    }, [width]);
+
+    React.useEffect(() => {
+        if (clickedCardTitle) {
+            history?.replaceState(
+                null,
+                "",
+                `/category/${clickedCardTitle?.category?.name}`
+            );
+        } else {
+            history?.replaceState(null, "", `/on-demand`);
+        }
+    }, [clickedCardTitle]);
+    const views = width
+        ? page?.movies
+            ? page?.movies[0].channels
+                  .map((el, index) =>
+                      index > 5 ? null : (
+                          <div key={el.title}>
+                              <div
+                                  className="carousel"
+                                  style={{
+                                      background: `linear-gradient(to bottom, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 39%, rgba(0, 0, 0, 1) 100%),url("${el.wallpaper}/${width}/500"), no-repeat`,
+                                  }}
+                              >
+                                  <div className="carousel-about">
+                                      <div className="carousel-title">
+                                          {el.title}
+                                      </div>
+                                      <div className="carousel-description">
+                                          {el.description}
+                                      </div>
+                                      <Button
+                                          inner={constants.WATCH_NOW}
+                                          onClick={() =>
+                                              launchPlayer(
+                                                  pdp.videoId
+                                                      ? pdp.videoId
+                                                      : pdp.seasons[0].cards[0]
+                                                            .id,
+                                                  currVideo
+                                              )
+                                          }
+                                      />
+                                  </div>
+                              </div>
+                          </div>
+                      )
+                  )
+                  .filter((view) => view)
+            : null
+        : null;
+
+    console.log("page", page);
+    return useObserver(() =>
+        !error ? (
+            clickedCardTitle ? (
+                <>
+                    <h1 className="noShow">
+                        {clickedCardTitle?.category?.name}
+                    </h1>
+                    <div className="clickedTitle">
+                        <div
+                            className="backButton"
+                            onClick={() => setClickedCardTitle(null)}
+                        >
+                            <InlineSVG type="backArrow" />
+                        </div>
+                        <span className="clickedHeader">
+                            {clickedCardTitle?.category?.name}
+                        </span>
+                    </div>
+                    <CardList
+                        key={clickedCardTitle.category.id}
+                        type="grid"
+                        data={clickedCardTitle}
+                    />
+                </>
+            ) : (
+                <>
+                    <h1 className="noShow">On Demand</h1>
+                    <Carousel views={views} className="carousel-container" />
+                    {genreNav && <GenreSelector type="movies" links={genreNav} />}
+                    <div className="overflowWrapper">
+                        {page.promos?.length > 0 ? (
+                            <CardList
+                                className="homePromo"
+                                type="promoSmall"
+                                useHeader={false}
+                                data={{ cards: page.promos }}
+                                onHeaderClick={setClickedCardTitle}
+                            />
+                        ) : null}
+
+                        {page.rails.map((rail) => (
+                            <CardList
+                                key={rail.category.id}
+                                type="title"
+                                data={rail}
+                                onHeaderClick={setClickedCardTitle}
+                            />
+                        ))}
+                    </div>
+                </>
+            )
+        ) : (
+            <Error404 />
+        )
+    );
+}
+Home.getLayout = getLayout;
+
+export const getServerSideProps = async ({ req, res }) => {
+    let { session, config } = await getSession(req, res);
+    const routes = [
+        "/api/dsp/homepage",
+        "/api/dsp/movies/US/ios",
+        "/api/dsp/company/available/genres/movies",
+    ];
+    const pageOptions = { req, res, routes, session, config };
+    const page = await pageBuilder(pageOptions);
+
+    let error = pageError([session, config, page]);
+    if (error) {
+        res.statusCode = 404;
+    }
+
+    return {
+        props: {
+            session: session || null,
+            config: config || null,
+            page: page || null,
+            error: error || false,
+            pageType: "home",
+            seoObj: {},
+        },
+    };
+};
