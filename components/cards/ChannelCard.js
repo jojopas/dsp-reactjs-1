70e4fcdda9c435d42a2ea96a80ea75dcc2d9d0d1
Piyush Mishra <piyush.mishra@rsystems.com>
2021-08-11 16:24:30 +0530

    Fixed search live channel cards

--- components/cards/ChannelCard.js
+++ components/cards/ChannelCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-import "./Card.less";
+import "./Cards.less";
 import { constants } from "../../config";
 import { StoreContext } from "../../store";
 
