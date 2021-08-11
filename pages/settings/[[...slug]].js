432628b076ead08324b790975e6f8c4f9b0ad97c
Piyush Mishra <piyush.mishra@rsystems.com>
2021-08-11 13:23:35 +0530

    Fixed UAT 188

--- pages/settings/[[...slug]].js
+++ pages/settings/[[...slug]].js
@@ -32,7 +32,7 @@ export default function Settings({
                         </p>
 
                         <ContactForm />
-                        <div className="build">Release 1.0 build 3324</div>
+                        <div className="build">Release 1.0 build 3325</div>
                     </div>
                 </div>
             </>
