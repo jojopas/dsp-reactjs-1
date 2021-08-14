db7ca404d9b8a554a6b9f1a4687b998ccb45c364
Baby Steps <web@babystepsweb.com>
2021-08-13 17:22:35 -0700

    fix removeEventListener

--- components/channel/channel.js
+++ components/channel/channel.js
@@ -593,7 +593,7 @@ export default function Channels({
             return () => {
                 clearTimeout(fsTimer);
                 document.removeEventListener('mousemove', throttledTestMouse);
-                document.addEventListener('scroll', throttledTestMouse);
+                document.removeEventListener('scroll', throttledTestMouse);
             };
         }, []);
     }
